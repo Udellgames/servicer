566e31f5f47cfae1dc64a0fc1de0cee73dabfd23
Nick Udell <nick@udellgames.com>
2015-06-17 12:36:42 +0100

    Moved ServiceLocator from a PCL because .NET 3.5 cannot support it.

--- Servicer/Properties/AssemblyInfo.cs
+++ Servicer/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -14,7 +13,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("78b02f30-3c62-40f0-b59c-88bf2bce8c25")]
 
 // Version information for an assembly consists of the following four values:
 //
