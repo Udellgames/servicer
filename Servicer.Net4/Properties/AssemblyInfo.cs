d633446793ca537f5647247b85ca696fe09b1e7a
nick udell <nick@udellgames.com>
2015-08-02 21:51:15 +0100

    Readded ruleset

--- Servicer.Net4/Properties/AssemblyInfo.cs
+++ Servicer.Net4/Properties/AssemblyInfo.cs
@@ -1,14 +1,14 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Servicer.Net4")]
+[assembly: AssemblyTitle("Servicer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Servicer.Net4")]
+[assembly: AssemblyProduct("Servicer")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
