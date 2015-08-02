190ac393e4a13affd2b6679edb4ff2cb4d255dc2
nick udell <nick@udellgames.com>
2015-08-02 21:05:08 +0100

    Added async/await support for .NET 4.5 targeting projects.

---
+++ Servicer.Net45/PromiseEventArgs.cs
@@ -0,0 +1,29 @@
+namespace Servicer
+{
+    using System;
+
+    /// <summary>
+    /// Event args for when a promise is fulfilled.
+    /// </summary>
+    /// <typeparam name="T">The type of value that fulfills this promise.</typeparam>
+    [Obsolete("Do not use promises in .NET 4+. Use Tasks with Service Locator's GetServiceAsync method instead.")]
+    public sealed class PromiseFulfilledEventArgs<T> : EventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PromiseFulfilledEventArgs{T}"/> class.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        public PromiseFulfilledEventArgs(T value)
+        {
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
+        public T Value { get; private set; }
+    }
+}
